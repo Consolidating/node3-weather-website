fd78e66891b45207d8a26877c05909ce547cbf66
= <51408991+Consolidating@users.noreply.github.com>
2020-10-05 23:06:08 -0400

    log

--- src/app.js
+++ src/app.js
@@ -19,12 +19,14 @@ const publicDirectoryPath = path.join(__dirname, "../public")
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
+hbs.registerPartials(partialsPath) //Takes argument to directory of partials 
+
 //Setup handlebars engine and views location
 app.set('view engine', 'hbs') //Sets express viewengine as hbs, express requires us to have the hbs template in views folder 
 app.set('views', viewsPath) //Sets views directory to custom directory 
 app.use(express.static(publicDirectoryPath)) //Express will run through and send directory. However, index.html has special name will be default 
 
-hbs.registerPartials(partialsPath) //Takes argument to directory of partials 
+
 
 app.get('',(req,res)=>{
     res.render('index',{
