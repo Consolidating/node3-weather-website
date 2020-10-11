31f9cccde07234bb5b2231fe7c751690b0892c92
= <51408991+Consolidating@users.noreply.github.com>
2020-10-10 21:04:44 -0400

    Feelslike

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,13 +7,15 @@ const forecast = (latitude, longitude, callback)=>{
 
     
     request( {url:weatherStackUrl, qs:{access_key:weatherStackApiKey, query: `${latitude}, ${longitude}`, units: 'm'}, json:true}, (error, {body})=>{
+
              if (error){
                  callback("Unable to connect to weather service!", undefined)
              } else if(body.error){
                  callback("Unable to find location", undefined)
              }else {
                  
-                 callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. There is ${body.current.precip}% chance of rain.`)
+                 callback(undefined, `${body.current.weather_descriptions}. It is currently ${body.current.temperature} degrees out. It feels like  ${body.current.feelslike} degrees `)
+
             
              }
          })
